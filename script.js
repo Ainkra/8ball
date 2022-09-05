18feb3984092afbf5d29494856b98d0f0543dcac
Paaz_ <felke2101@gmail.com>
2022-09-05 14:34:33 +0200

    Update() Add DOM selectors

---
+++ script.js
@@ -0,0 +1,8 @@
+//#######################
+//      SELECTOR
+//#######################
+
+const questions = document.querySelector(".questions");
+const inputs    = document.querySelector("input");
+let answer      = document.querySelector(".answer")
+let body        = document.querySelector("body")
