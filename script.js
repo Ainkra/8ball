0f3cc35d3aba0c9c34701499cad06257ac40f33d
Paaz_ <felke2101@gmail.com>
2022-09-05 14:37:37 +0200

    Update() Add event listener

--- script.js
+++ script.js
@@ -4,5 +4,13 @@
 
 const questions = document.querySelector(".questions");
 const inputs    = document.querySelector("input");
-let answer      = document.querySelector(".answer")
-let body        = document.querySelector("body")
+let answer      = document.querySelector(".answer");
+let body        = document.querySelector("body");
+
+//#######################
+//      FUNCTION
+//#######################
+
+inputs.addEventListener("keypress", (e) => {
+
+});
